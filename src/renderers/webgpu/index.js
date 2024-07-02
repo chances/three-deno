9a993990a4d4df3e7236b2e8b6f0c03bfb95cd7f
Chance Snow <git@chancesnow.me>
2024-07-02 18:29:18 +0000

    Add an entry point to WebGPU renderer types

---
+++ src/renderers/webgpu/index.js
@@ -0,0 +1,12 @@
+export { default as WebGPUBackend } from './WebGPUBackend.js';
+export { default as WebGPURenderer } from './WebGPURenderer.js';
+
+import * as Constants from './utils/WebGPUConstants.js';
+export { Constants };
+
+export { default as WebGPUAttributeUtils } from './utils/WebGPUAttributeUtils.js';
+export { default as WebGPUBindingUtils } from './utils/WebGPUBindingUtils.js';
+export { default as WebGPUPipelineUtils } from './utils/WebGPUPipelineUtils.js';
+export { default as WebGPUTexturePassUtils } from './utils/WebGPUTexturePassUtils.js';
+export { default as WebGPUTextureUtils } from './utils/WebGPUTextureUtils.js';
+export { default as WebGPUUtils } from './utils/WebGPUUtils.js';
